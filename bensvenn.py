600a4eda05e3f1fafa98802b0182cd8e16364a36
Benjamin VanderSluis <ben@ourapartment.com>
2019-03-31 17:47:53 -0500

    async bug, numeric ids now primary
    Now using rewrite branch of discord.py, cloned from github and installed
    w/ pip. So everything is compatible with python > 3.5. I also renamed
    the main file, and redid the user mapping logic. Everything is done
    with numeric ids now. You have to make a mapping from one to the
    other, but there are two dumping utilities to help.

---
+++ bensvenn.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3 
+
+import discord
+import random
+import asyncio
+import vennsteam as vs
+import tokens
+import pandas as pd
+
+
+class MyClient(discord.Client):
+   async def on_ready(self):
+      print('Logged in as ' + self.user.name)
+      print('ID is ' + str(self.user.id))
+      print('------')
+
+
+   async def on_message(self, message):
+      # we do not want the bot to reply to itself
+      if message.author.id == self.user.id:
+         return
+
+      if message.content.startswith('$guess'):
+         await message.channel.send('Guess a number between 1 and 10.')
+
+         def is_correct(m):
+            return m.author == message.author and m.content.isdigit()
+
+         answer = random.randint(1, 10)
+
+         try:
+            guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+         except asyncio.TimeoutError:
+            return await message.channel.send('Sorry, you took too long it was {}.'.format(answer))
+
+         if int(guess.content) == answer:
+            await message.channel.send('You are right!')
+         else:
+            await message.channel.send('Oops. It is actually {}.'.format(answer))
+
+      elif message.content.startswith('?bensvenn'):
+         # parse command to get target channel, defaut to General
+         target_channel = message.content[len('?bensvenn'):].strip()
+         target_handle = None
+         steamids = []
+
+         if target_channel == '':
+            target_channel = 'General'
+
+          # find a handle to target_channel
+         for c in client.get_all_channels():
+            if c.name == target_channel:
+               target_handle = c
+               break
+
+         # check channel and population
+         if target_handle is None:
+            await message.channel.send('no such voice channel %s' % target_channel)
+            return
+
+         if len(target_handle.members) == 0:
+            await message.channel.send("nobody's home in %s!" % target_channel)
+            return
+
+         # convert discord ids to steam ids
+         for usr in target_handle.members:
+            usr_id = str(usr.id)
+            if usr_id in dis_to_stm.index:
+               steamids.append(dis_to_stm.loc[usr_id,'steam_id'])
+            else:
+               await message.channel.send('No steamID registered for %s' % usr.name)
+               continue
+
+         # currently no on is in the mapping that's not my friend, but that could change
+         for sid in steamids:
+            if sid not in my_friends:
+               steam_nick = vs.get_names(steam_id=str(sid))
+               steamids.remove(sid)
+               await message.channel.send('%s not steam-friends with WhileTrueGame' % steam_nick[sid])
+               continue
+
+
+         if len(steamids) < len(target_handle.members):
+            await message.channel.send('(Only got games for %d people)' % len(steamids))
+
+         if len(steamids) > 0:
+            gameset = vs.venn_games(steam_ids=','.join(steamids), app_names=app_names)
+
+            report_all = 'Games everyone in %s has:\n' %target_channel 
+            report_all += '\n'.join(gameset[len(steamids)])
+
+            if len(report_all) > 2000:
+               await message.channel.send('Character limit exceeded, this list will be abridged')
+               report_all = report_all[:2000]
+
+            await message.channel.send(report_all)
+
+
+      elif message.content.startswith('?debug'):
+         print('enter debug')
+         import ipdb; ipdb.set_trace()
+
+         print('exit debug')
+
+
+   def dump_guild(self):
+      G = self.guilds[0]
+      fid = open('guild_dump.tsv', 'w')
+      fields = ('Member id', 'name', 'discriminator', 'nick')
+      print('%s\t%s\t%s\t%s' % fields, file=fid)
+
+      for m in G.members:
+         fields = (m.id, m.name, m.discriminator, m.nick)
+         print('%s\t%s\t%s\t%s' % fields, file=fid)
+
+      fid.close()
+
+
+
+
+# </Class definition>
+
+# try loading some global lookup tables here ---------------
+
+# discord to steam name map, make them all strings
+dis_to_stm = pd.read_table('user_database.tsv', index_col=0)
+dis_to_stm.index = dis_to_stm.index.astype(str)
+dis_to_stm.steam_id = dis_to_stm.astype(str)
+
+# steam name to steam id map
+my_friends = vs.get_my_friends()
+
+# appid to game name map
+app_names = vs.get_app_list()
+
+# Loaded. Ready. Run!   ------------------------------------
+
+
+client = MyClient()
+client.run(tokens.vennbot_token)
--- vennsteam.py
+++ vennsteam.py
@@ -4,140 +4,164 @@ import tokens
 
 api_url_base = 'http://api.steampowered.com/'
 
+
+def dump_friends():
+   """ dump a list of all my friends, their IDs and their alias to a flat file"""
+
+   friend_list = get_my_friends()
+
+   fid = open('steam_friend_dump.tsv', 'w')
+   fields = ('steamid', 'steamnick')
+   print('%s\t%s' % fields, file=fid)
+   for id, nick in friend_list.items():
+      print('%s\t%s' % (id, nick), file=fid)
+
+   fid.close()
+
+
+
 def get_my_friends():
-    # return a listof my friends steamids
-    # add myself to the list
+   """ 
+   Return a dict of all my friends steamID -> handles
+   BOTH are STRINGS
+   """
 
-    steam_id = tokens.benny
+   steam_id = tokens.benny
 
-    friends_list_url = api_url_base + 'ISteamUser/GetFriendList/v0001/?'
-    friends_list_url += 'key=' + tokens.steam_api_token + '&'
-    friends_list_url += 'steamid=' + steam_id + '&'
-    friends_list_url += 'format=json'
+   friends_list_url = api_url_base + 'ISteamUser/GetFriendList/v0001/?'
+   friends_list_url += 'key=' + tokens.steam_api_token + '&'
+   friends_list_url += 'steamid=' + steam_id + '&'
+   friends_list_url += 'format=json'
 
-    response = requests.get(friends_list_url)
-    if not response.ok:
-        print('error with request:')
-        print(response.content.decode('utf-8'))
-        return None
+   response = requests.get(friends_list_url)
+   if not response.ok:
+      print('error with request:')
+      print(response.content.decode('utf-8'))
+      return None
 
-    rdict = json.loads(response.content.decode('utf-8'))
+   rdict = json.loads(response.content.decode('utf-8'))
 
-    friends =  [x['steamid'] for x in rdict['friendslist']['friends']]
-    friends.append(tokens.benny)
-    return friends
+   # unpack the list of steam ids
+   friends = [x['steamid'] for x in rdict['friendslist']['friends']]
 
+   # add myself to the list
+   friends.append(tokens.benny)
 
-def get_all_friends_names(friends=None):
-    """ convert steam persona names to steamids, using my friendslist
-        friends is a list of steamids"""
-    if friends is None:
-        friends = get_my_friends()
+   # lookup the name for each
+   steamid_to_name = get_names(steam_id = ','.join(friends))
 
-    name_to_steamid = get_names(steam_id=','.join(friends))
-    return name_to_steamid
+   return steamid_to_name 
 
 
 def get_names(steam_id=None):
-    # lookup steam handles from steam ids
-    # pass in a string
-    # can also be a comma delimited string-list
-    # output is a dict of handle -> steam id
+   """ return a dict of steam_ids -> names 
+      steam_id can be a comma delimited string
+      the API takes such a string
+   """
+   # lookup steam handles from steam ids
+   # pass in a string
+   # can also be a comma delimited string-list
+   # output is a dict of handle -> steam id
 
-    if steam_id is None:
-        steam_id = tokens.matty
-        steam_id += ',' + tokens.benny
+   if steam_id is None:
+      steam_id = tokens.matty
+      steam_id += ',' + tokens.benny
 
-    profile_list_url = api_url_base + 'ISteamUser/GetPlayerSummaries/v0002/?' 
-    profile_list_url += 'key=' + tokens.steam_api_token + '&'
-    profile_list_url += 'steamids=' + steam_id + '&'
-    profile_list_url += 'format=json'
-    response = requests.get(profile_list_url)
+   profile_list_url = api_url_base + 'ISteamUser/GetPlayerSummaries/v0002/?' 
+   profile_list_url += 'key=' + tokens.steam_api_token + '&'
+   profile_list_url += 'steamids=' + steam_id + '&'
+   profile_list_url += 'format=json'
+   response = requests.get(profile_list_url)
 
-    if not response.ok:
-        print('error with request:')
-        print(response.content.decode('utf-8'))
-        return None
+   if not response.ok:
+      print('error with request:')
+      print(response.content.decode('utf-8'))
+      return None
 
-    rdict = json.loads(response.content.decode('utf-8'))
+   rdict = json.loads(response.content.decode('utf-8'))
 
-    name_to_steamid = {}
-    for p in rdict['response']['players']:
-        name_to_steamid[p['personaname']] = p['steamid']
+   steamid_to_name = {}
+   for p in rdict['response']['players']:
+      steamid_to_name[p['steamid']] = p['personaname']
 
-    return name_to_steamid
+   return steamid_to_name
 
 
 def get_games(steam_id=None):
-    # get list of appids owned by single steamid
-    if steam_id is None:
-        steam_id = tokens.matty
+   # get list of appids owned by single steamid
+   if steam_id is None:
+      steam_id = tokens.matty
 
-    game_list_url = api_url_base + 'IPlayerService/GetOwnedGames/v0001/?' 
-    game_list_url += 'key=' + tokens.steam_api_token + '&'
-    game_list_url += 'steamid=' + steam_id + '&'
-    game_list_url += 'format=json'
+   game_list_url = api_url_base + 'IPlayerService/GetOwnedGames/v0001/?' 
+   game_list_url += 'key=' + tokens.steam_api_token + '&'
+   game_list_url += 'steamid=' + steam_id + '&'
+   game_list_url += 'format=json'
 
-    response = requests.get(game_list_url)
-    if not response.ok:
-        print('error with request:')
-        print(response.content.decode('utf-8'))
-        return None
+   response = requests.get(game_list_url)
+   if not response.ok:
+      print('error with request:')
+      print(response.content.decode('utf-8'))
+      return None
 
-    rdict = json.loads(response.content.decode('utf-8'))
-    games = [x['appid'] for x in rdict['response']['games']]
-    return games
+   rdict = json.loads(response.content.decode('utf-8'))
+   games = [x['appid'] for x in rdict['response']['games']]
+   return games
 
 
 def get_app_list():
-    # get the whole list so we can map appids to names
-    #  http://api.steampowered.com/ISteamApps/GetAppList/v0002/
-    # returns { appid (int)  -> name dict }
-    app_list_url = api_url_base + 'ISteamApps/GetAppList/v0002/?'
-    app_list_url += 'format=json'
+   # get the whole list so we can map appids to names
+   #  http://api.steampowered.com/ISteamApps/GetAppList/v0002/
+   # returns { appid (int)  -> name dict }
+   app_list_url = api_url_base + 'ISteamApps/GetAppList/v0002/?'
+   app_list_url += 'format=json'
 
-    response = requests.get(app_list_url)
-    if not response.ok:
-        print('error with request:')
-        print(response.content.decode('utf-8'))
-        return None
+   response = requests.get(app_list_url)
+   if not response.ok:
+      print('error with request:')
+      print(response.content.decode('utf-8'))
+      return None
 
-    rdict = json.loads(response.content.decode('utf-8'))
-    app_names = {}
+   rdict = json.loads(response.content.decode('utf-8'))
+   app_names = {}
 
-    for app in rdict['applist']['apps']:
-        app_names[app['appid']] = app['name']
-
-
-    return app_names
+   for app in rdict['applist']['apps']:
+       app_names[app['appid']] = app['name']
 
 
-def venn_games(steam_ids=None, app_names=None):
-    # take in a comma sep list of ids, 
-    # returns a game set dict
-    # keys are ints (how many players own)
-    # values are each a list of games
+   return app_names
 
-    if steam_ids is None:
-        steam_ids = '76561197961737526' # benny
-        steam_ids += ',76561197970805046' # matty
 
-    if app_names is None:
-        app_names = get_app_list()
+def venn_games(steam_ids=None, app_names=None, exclude=None):
+   # take in a comma sep list of ids, 
+   # returns a game set dict
+   # keys are ints (how many players own)
+   # values are each a list of games
 
-    appid_list = []
-    for id in steam_ids.split(','):
-        appid_list.extend(get_games(steam_id=id))
+   if steam_ids is None:
+      steam_ids = '76561197961737526' # benny
+      steam_ids += ',76561197970805046' # matty
 
-    appid_set = list(set(appid_list))
-    appid_count = [appid_list.count(x) for x in appid_set]
+   if exclude is not None:
+      for e in eclude.split(','):
+         if e in steam_ids:
+            steam_ids.remove(e)
 
-    game_set_dict = {}
-    for app,score in zip(appid_set, appid_count):
-        if score not in game_set_dict:
-            game_set_dict[score] = [app_names[app]]
-        else:
-            game_set_dict[score].append(app_names[app])
+   if app_names is None:
+      app_names = get_app_list()
 
-    return game_set_dict
+   appid_list = []
+   for id in steam_ids.split(','):
+      appid_list.extend(get_games(steam_id=id))
+
+   appid_set = list(set(appid_list))
+   appid_count = [appid_list.count(x) for x in appid_set]
+
+   game_set_dict = {}
+   for app,score in zip(appid_set, appid_count):
+      if score not in game_set_dict:
+         game_set_dict[score] = [app_names[app]]
+      else:
+         game_set_dict[score].append(app_names[app])
+
+   return game_set_dict
 
